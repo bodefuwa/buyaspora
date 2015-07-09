01bf6a8163c947e8770c1e6b930035408d72dcb7
Bode Fuwa <bodefuwa@gmail.com>
2015-07-09 15:20:59 -0400

    fixed static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
+
+  resources :business_details
+  resources :businesses
   resources :sub_cultures
   resources :cultures
   resources :sub_categories
   resources :categories
   resources :category_types
-  # get => 'pages/home'
-  get 'about'   => 'pages/about'
-  get 'contact' => 'pages/contact'
-  get 'terms'   => 'pages/terms'
-  get 'privacy' => 'pages/privacy'
-  get 'faqs'    => 'pages/faqs'
-  get 'help'    => 'pages/help'
+  
+  get 'about'   => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'terms'   => 'pages#terms'
+  get 'privacy' => 'pages#privacy'
+  get 'faqs'    => 'pages#faqs'
+  get 'help'    => 'pages#help'
 
-  resources :business_details
-  resources :businesses
   root 'pages#home'
 end
